688ddca731b361909e9be00fa4673e7fe1c6171a
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-10-28 19:42:28 +0500

    Parameter Passing Programs

---
+++ ParameterPassing.java
@@ -0,0 +1,9 @@
+class ParameterPassing{
+	public static void pass(int a){
+	System.out.println("Passing the parameter The Value");
+	System.out.println("The parameter value is"+ a);}
+	public static void main(String args[]){
+	System.out.println("Pass The parameteric value");
+	pass(5);	}
+
+}
